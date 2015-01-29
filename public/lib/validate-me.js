7e69088569da2afe6552af24b60c76342c80c8f2
tahmina8765 <tahmina8765@yahoo.com>
2015-01-29 19:02:12 +0600

    add new validation rules

--- public/lib/validate-me.js
+++ public/lib/validate-me.js
@@ -10,12 +10,13 @@ var validateMeModule = (function () {
 
     var formId = '';
     var fields = {};
-    var formObj;
     var mode = 'dev'; // dev|prod
+    var cssErrorClass = 'validate-me-error';
 
     var message = {
         'formNotExist': 'Form Does not exist, Validate Me can not validate you :(',
-        'empty': 'This field can not be empty',
+        'nonempty': 'This field can not be empty',
+        'equelto': 'Equal to',
         'email': 'Invalid email address'
     };
 
@@ -38,6 +39,33 @@ var validateMeModule = (function () {
         }
         return indexOf.call(this, needle);
     };
+    function _removeErrorMessages() {
+        var paras = document.getElementsByClassName(cssErrorClass);
+        while (paras[0]) {
+            paras[0].parentNode.removeChild(paras[0]);
+        }
+    }
+
+    function _displayError(fieldId, rule, displayType) {
+        var element = document.getElementById(fieldId);
+        var errorMessage = 'Invalid Information.';
+        if(message[rule]){
+            errorMessage = message[rule];
+        }
+        switch (displayType) {
+            case '1':
+                var para = document.createElement("div");
+                para.className = para.className + " "+cssErrorClass+" rule";
+                var node = document.createTextNode(errorMessage);
+                para.appendChild(node);
+                element.parentNode.appendChild(para);
+                break;
+            default:
+                // Remove Message if exist
+                break;
+        }
+
+    }
 
     var validate = {
         'nonempty': function (fieldValue) {
@@ -60,7 +88,7 @@ var validateMeModule = (function () {
         },
         'regex': function (fieldValue, expression) {
             console.log(expression);
-            var re = new RegExp('^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+            var re = new RegExp('\/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$\/');
             var result = re.test(fieldValue);
             console.log(result);
             return result;
@@ -85,32 +113,55 @@ var validateMeModule = (function () {
     }
 
     function _validateFields() {
+        _removeErrorMessages();
         var validtionResult = [];
+        var isValid = '';
+
         if (fields) {
             for (var key in fields) {
                 if (fields.hasOwnProperty(key)) {
                     var fieldValue = document.getElementById(key).value;
                     var type = (typeof fields[key]);
                     if (type === 'string') {
-                        validtionResult.push(_validationUnit(fields[key], fieldValue));
+                        isValid = _validationUnit(fields[key], fieldValue);
+                        validtionResult.push(isValid);
+                        if (!isValid) {
+                            _displayError(key, fields[key], '1');
+                        } else {
+                            _displayError(key);
+                        }
+                        _shout('Validate ' + key + ' with ' + fields[key] + ' = ' + isValid);
                     }
                     if (type === 'object') {
                         for (var rule in fields[key]) {
+                            isValid = _validationUnit(rule, fieldValue, fields[key][rule]);
+                            validtionResult.push(isValid);
+                            if (!isValid) {
+                                _displayError(key, rule, '1');
+                            } else {
+                                _displayError(key);
+                            }
+                            _shout('Validate ' + key + ' with ' + rule + ' -> ' + fields[key][rule] + ' = ' + isValid);
+//                            
 //                            console.log(rule + ':' + fields[key][rule] + ':' + fieldValue);
-                            validtionResult.push(_validationUnit(rule, fieldValue, fields[key][rule]));
+//                            validtionResult.push(_validationUnit(rule, fieldValue, fields[key][rule]));
                         }
                     }
                 }
             }
+
         }
-        if(indexOf.call(validtionResult, false) < 0){
+
+        if (indexOf.call(validtionResult, false) < 0) {
             return true;
         }
+
         console.log(validtionResult);
+
         return false;
     }
 
-    function _submit(e) {
+    function _submit(e) {        
         var isValid = _validateFields();
         if (!isValid) {
             alert("Please check your fields!");
@@ -148,7 +199,9 @@ var validateMeModule = (function () {
 
 
     function _shout(message) {
-        alert(message);
+        if (mode === 'dev') {
+            console.log(message);
+        }
     }
 
     function _log() {
